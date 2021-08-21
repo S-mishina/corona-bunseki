8556769c91333884d2683e3c86fa56cfd35f922f
S-mishina <183244r4@stu.teikyo-u.ac.jp>
2021-08-21 22:19:47 +0900

    bashにpip instalを追加

--- corona_bash.sh
+++ corona_bash.sh
@@ -6,6 +6,7 @@ git pull
 #ファイル名をここで決める
 to_day=$(date +"%Y%m%d")
 echo $to_day
-
+#pipの更新
+pip install -r requirements.txt
 #python jupiter出力
 jupyter nbconvert --execute COVID.ipynb --output $to_day  --to html
