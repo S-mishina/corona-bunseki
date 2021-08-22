35894d28e540e0bd9d512c00e75d33e5fd91fa6f
S-mishina <183244r4@stu.teikyo-u.ac.jp>
2021-08-23 03:32:22 +0900

    html　形式

--- corona_bash.sh
+++ corona_bash.sh
@@ -9,6 +9,6 @@ echo $to_day
 #pipの更新
 pip3 install -r ../../home/ssm-user/corona-bunseki/requirements.txt
 #python jupiter出力
-jupyter nbconvert --execute ../../home/ssm-user/corona-bunseki/test.ipynb --output output/$to_day  --to pdf
+jupyter nbconvert --execute ../../home/ssm-user/corona-bunseki/test.ipynb --output output/$to_day  --to html
 #aws s3アップロード
 aws s3 cp ../../home/ssm-user/corona-bunseki/output s3://corona-out-put-log/ --recursive
