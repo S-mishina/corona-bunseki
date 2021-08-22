405b6c73dac77e9fed62a615dda1b29afaaeb502
S-mishina <183244r4@stu.teikyo-u.ac.jp>
2021-08-23 02:17:06 +0900

    pip listの追加

--- corona_bash.sh
+++ corona_bash.sh
@@ -7,7 +7,7 @@ git pull
 to_day=$(date +"%Y%m%d")
 echo $to_day
 #pipの更新
-pip3 install -r requirements.txt
+pip3 install -r ../../home/ssm-user/corona-bunseki/requirements.txt
 #python jupiter出力
 jupyter nbconvert --execute ../../home/ssm-user/corona-bunseki/test.ipynb --output output/$to_day  --to html
 #aws s3アップロード
